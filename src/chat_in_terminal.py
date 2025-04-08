ad4dd582a543022defe979352ec515b0697526fb
Far-R <farzad.roozitalab@gmail.com>
2025-04-07 21:28:46 -0600

    update chat in terminal

--- src/chat_in_terminal.py
+++ src/chat_in_terminal.py
@@ -4,10 +4,20 @@ from utils.chatbot_agentic_v2 import Chatbot as Chatbot_v2
 from utils.chatbot_agentic_v3 import Chatbot as Chatbot_v3
 
 # If you'd like to chat with a different chatbot, modify the code manually.
+chatbot_version = "basic"
+# chatbot_version = "v2"
+# chatbot_version = "v3"
 
 if __name__ == "__main__":
-    chatbot = Chatbot_v3()
-    print("Chatbot initialized. Type 'exit' to end the conversation.")
+    if chatbot_version == "basic":
+        print("Basic chatbot is initialized. Type 'exit' to end the conversation.")
+        chatbot = Chatbot()
+    elif chatbot_version == "v2":
+        print("Chatbot-agentic-v2 is initialized.  Type 'exit' to end the conversation.")
+        chatbot = Chatbot_v2()
+    elif chatbot_version == "v3":
+        print("Chatbot-agentic-v3 is initialized.  Type 'exit' to end the conversation.")
+        chatbot = Chatbot_v3()
 
     while True:
         user_input = input("\nYou: ")
