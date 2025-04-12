514ec7c07315919bf8d129032672273da4d14f7a
Far-R <farzad.roozitalab@gmail.com>
2025-04-12 15:32:07 -0600

    switch chat_in_terminal

--- src/chat_in_terminal.py
+++ src/chat_in_terminal.py
@@ -4,8 +4,8 @@ from utils.chatbot_agentic_v2 import Chatbot as Chatbot_v2
 from utils.chatbot_agentic_v3 import Chatbot as Chatbot_v3
 
 # If you'd like to chat with a different chatbot, modify the code manually.
-chatbot_version = "basic"
-# chatbot_version = "v2"
+# chatbot_version = "basic"
+chatbot_version = "v2"
 # chatbot_version = "v3"
 
 if __name__ == "__main__":
